127dff58baf4968a6d030e70bf51bd7d67bcad40
Ibnathism <1605106@ugrad.cse.buet.ac.bd>
2020-04-29 19:09:00 +0600

    work with navlink

---
+++ client/src/components/AppNavbar.js
@@ -0,0 +1,62 @@
+import React, { Component } from 'react';
+import {
+    Collapse,
+    Navbar,
+    NavbarToggler,
+    NavbarBrand,
+    Nav,
+    NavItem,
+    NavLink,
+    Container
+} from 'reactstrap';
+
+class AppNavbar extends Component {
+    //One way
+    /*constructor(props) {
+        super(props);
+        
+        //this is the alternate of the arrow function
+        //this.toggle = this.toggle.bind(this);
+
+        this.state = {
+            isOpen: false
+
+        }
+    }*/
+
+    //other way
+    state = {
+        isOpen: false
+    }
+
+
+    toggle = () =>  {
+        this.setState({
+            isOpen: !this.state.isOpen
+        });
+    }    
+    render() {
+        return(
+        <div>
+            <Navbar color="dark" dark expand="sm" className="mb-5">
+                <Container>
+                    <NavbarBrand href="/">Todo List</NavbarBrand>
+                    <NavbarToggler onClick={this.toggle}/>
+                    <Collapse isOpen={this.state.isOpen} navbar>
+                        <Nav className="ml-auto" navbar>
+                            <NavItem>
+                                <NavLink href="https://github.com/ibnathism">
+                                    Github
+                                </NavLink>
+                            </NavItem>
+                        </Nav>
+                    </Collapse>
+                </Container>
+            </Navbar>
+        </div>
+        )
+    }
+}
+
+
+export default AppNavbar;
